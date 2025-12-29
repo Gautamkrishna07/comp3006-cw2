45bbc10caf3498ececb353a8e1baf94939aca22e
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-29 19:34:24 +0000

    Added login hook and page

---
+++ SourceCode/frontend/src/hooks/useLogin.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+const useLogin = () => {
+    const [ error, setError ] = useState(null);
+    const [ isLoading, setIsLoading ] = useState(false);
+    const { dispatch } = useAuthContext();
+
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const baseUrl = process.env.REACT_APP_API_BASE_URL || "/api";
+
+        try {
+            const response = await fetch(`${baseUrl}/users/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({ email, password })
+            });
+
+            const json = await response.json();
+
+            if (!response.ok) {
+                setIsLoading(false);
+                setError(json.error);
+                return;
+            }
+
+            localStorage.setItem("user", JSON.stringify(json));
+            dispatch({ type: "LOGIN", payload: json });
+        } catch (e) {
+            console.error(e);
+            setError(e.message);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+    
+    return { login, isLoading, error };
+}
+
+export default useLogin;
--- SourceCode/frontend/src/pages/Login.jsx
+++ SourceCode/frontend/src/pages/Login.jsx
@@ -1,6 +1,48 @@
+import { useState } from "react";
+import useLogin from "../hooks/useLogin";
+
 const Login = () => {
+
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const { login, error, isLoading } = useLogin();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await login(email, password);
+    };
+
     return ( 
-        <></>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <h3>Login</h3>
+
+                <label htmlFor="email">Email:</label>
+                <input 
+                    type="email" 
+                    name="email" id="email"
+                    onChange={(e) => { setEmail(e.target.value)}}
+                    value={email}
+                    required
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input 
+                    type="password" 
+                    name="password" id="password"
+                    onChange={(e) => { setPassword(e.target.value)}}
+                    value={password}
+                    required
+                />
+
+                <button disabled={isLoading} type="submit">
+                    { isLoading ? "Logging in..." : "Login" }
+                </button>
+
+                { error && <p className="error">{ error }</p> }
+            </form>
+        </div>
     );
 }
  
