bfc4120708e963252e0cb732f885890a91170a96
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-29 19:20:45 +0000

    Added logout hook and button to navbar

--- SourceCode/frontend/src/components/Navbar.jsx
+++ SourceCode/frontend/src/components/Navbar.jsx
@@ -1,26 +1,33 @@
 import { Link } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { useLogout } from "../hooks/useLogout";
+
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
     const { user } = useAuthContext();
+    const { logout } = useLogout();
     
     return ( 
         <header className={styles.header}>
             <div className={styles.navbarContainer}>
                 <h1 className={styles.title}>Social Media App</h1>
             
-                <nav>
+                <nav className={styles.navLinks}>
                     { user ? (
-                        <div className={styles.navLinks}>
-                            <Link to="/">Home Feed</Link>
-                            <Link to={`/profile/${user.username}`}>{user.username}</Link>
-                        </div>
+                        <>
+                            <Link className={styles.navItem} to="/">Home Feed</Link>
+                            <Link className={styles.navItem} to={`/profile/${user.username}`}>{user.username}</Link>
+
+                            <button className={styles.navItem} onClick={logout}>
+                                Log out
+                            </button>
+                        </>
                     ) : (
-                        <div className={styles.navLinks}>
-                            <Link to="/login">Login</Link>
-                            <Link to="/signup">Signup</Link>
-                        </div>
+                        <>
+                            <Link className={styles.navItem} to="/login">Login</Link>
+                            <Link className={styles.navItem} to="/signup">Signup</Link>
+                        </>
                     )}
                 </nav>
             </div>
