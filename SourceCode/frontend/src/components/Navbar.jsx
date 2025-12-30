9db200e8e1a8978237334f099e39589603487972
Corey Richardson <richardsoncorey04@gmail.com>
2025-12-30 23:06:52 +0000

    Improved accessibility of navbar iconography

--- SourceCode/frontend/src/components/Navbar.jsx
+++ SourceCode/frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useAuthContext } from "../hooks/useAuthContext";
 import useLogout from "../hooks/useLogout";
-import { LogOut  } from "lucide-react";
+import { House, LogOut, UserRound } from "lucide-react";
 import clsx from "clsx";
 import styles from "../styles/Navbar.module.css";
 
@@ -20,14 +20,23 @@ const Navbar = () => {
                             <NavLink 
                                 className={({ isActive }) => clsx(styles.navItem, { [styles.active]: isActive })}
                                 to="/"
-                            >Home Feed</NavLink>
+                                aria-label="Home feed"
+                                title="Explore the home feed"
+                            >{<House size={20} />} &nbsp; Home Feed</NavLink>
 
                             <NavLink 
                                 className={({ isActive }) => clsx(styles.navItem, { [styles.active]: isActive })}
                                 to={`/profile/${user.username}`}
-                            >{user.username}</NavLink>
+                                aria-label="Your profile page"
+                                title="Your profile"
+                            >{<UserRound size={20} />} &nbsp; {user.username}</NavLink>
 
-                            <button className={clsx(styles.navItem)} onClick={logout}>
+                            <button 
+                                className={clsx(styles.navItem)} 
+                                onClick={logout}
+                                aria-label="Log out of your account"
+                                title="Log out of your account"
+                            >
                                 <LogOut size={20}/>
                             </button>
                         </>
